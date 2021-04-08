1f03d7390b2fdd433f3891065757058e6eb1c48a
titar <dmitry.titar@ligastavok.ru>
2021-04-08 20:55:33 +0300

    fixed selenoid setting

--- src/test/java/scenarios/TestBase.java
+++ src/test/java/scenarios/TestBase.java
@@ -33,6 +33,6 @@ public class TestBase {
         capabilities.setCapability("enableVNC", true);
         capabilities.setCapability("enableVideo", true);
         Configuration.browserCapabilities = capabilities;
-        Configuration.remote = "https://c05-dtitar:vnyc6rsb8y$@selenoid.autotests.cloud/wd/hub/";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
     }
 }
